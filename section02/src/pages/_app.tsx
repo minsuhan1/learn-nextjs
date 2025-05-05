74e1efd8dc7ee8f7efec847c7af09e345ab8f2a6
minsuhan1 <trms7794han@gmail.com>
2025-05-05 23:58:01 +0900

    feat: 🎸 페이지별 레이아웃 설정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
 
 // This is the main entry point for a Next.js application
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <GlobalLayout>
-      <Component {...pageProps} />
-    </GlobalLayout>
-  );
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: NextPageWithLayout }) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
